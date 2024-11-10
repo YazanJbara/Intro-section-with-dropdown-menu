355cac6ff4db8723da8cffcf8894ce15f51add40
Yazan <yazanjbara20034456@gmail.com>
2024-11-10 03:12:01 +0300

    project is done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('.arrow').forEach(arrow => {
+document.querySelectorAll('.arrow').forEach((arrow) => {
   const arrowDown = arrow.querySelector('.arrow_down');
   const arrowUp = arrow.querySelector('.arrow_up');
   const dropdown = arrow.nextElementSibling;
@@ -16,3 +16,29 @@ document.querySelectorAll('.arrow').forEach(arrow => {
   });
 });
 
+const mobileMenuIcon = document.querySelector('.mobile_menu .menu');
+const mobileNavLinks = document.querySelector('.nav_links.__mobile');
+const closeMenuIcon = document.querySelector('.mobile_menu .close_menu');
+const overlay = document.querySelector('.overlay');
+
+mobileMenuIcon.addEventListener('click', () => {
+  mobileNavLinks.classList.toggle('show');
+  overlay.classList.toggle('show');
+  mobileMenuIcon.style.display = 'none';
+  closeMenuIcon.style.display = 'block';
+});
+
+closeMenuIcon.addEventListener('click', () => {
+  mobileNavLinks.classList.remove('show');
+  overlay.classList.remove('show');
+  mobileMenuIcon.style.display = 'block';
+  closeMenuIcon.style.display = 'none';
+});
+
+const mobileCloseIcon = document.querySelector('.nav_links.__mobile .close_menu');
+mobileCloseIcon.addEventListener('click', () => {
+  mobileNavLinks.classList.remove('show');
+  overlay.classList.remove('show');
+  mobileMenuIcon.style.display = 'block';
+  closeMenuIcon.style.display = 'none';
+});
